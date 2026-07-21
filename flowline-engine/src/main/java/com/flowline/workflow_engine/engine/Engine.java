b392fe5c07d524ee346be8189cfe371d3fa0348d
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2026-07-21 20:49:41 +0530

    Added persistence of steps in db from service and engine

--- flowline-engine/src/main/java/com/flowline/workflow_engine/engine/Engine.java
+++ flowline-engine/src/main/java/com/flowline/workflow_engine/engine/Engine.java
@@ -3,8 +3,10 @@ package com.flowline.workflow_engine.engine;
 import com.flowline.workflow_engine.engine.models.ProgressMessage;
 import com.flowline.workflow_engine.engine.models.Step;
 import com.flowline.workflow_engine.engine.enums.StepStatus;
+import com.flowline.workflow_engine.engine.models.StepEntity;
 import com.flowline.workflow_engine.engine.models.WorkflowResult;
 import com.flowline.workflow_engine.handlers.ProgressWebSocketHandler;
+import com.flowline.workflow_engine.repos.StepRepository;
 import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
@@ -17,20 +19,34 @@ public class Engine {
 
     private final ProgressWebSocketHandler handler;
 
-    public WorkflowResult run(List<Step> steps) {
+    private final StepRepository stepRepository;
+
+    public WorkflowResult run(List<Step> steps, List<StepEntity> entities) {
         Map<String, StepStatus> statuses = new LinkedHashMap<>();
 
-        for (Step step : steps) {
+        for (int i=0; i<steps.size(); i++) {
+            Step step = steps.get(i);
+            StepEntity entity = entities.get(i);
+
             try {
                 statuses.put(step.getName(), StepStatus.RUNNING);
                 handler.sendUpdate(new ProgressMessage(step.getName(), StepStatus.RUNNING));
+                entity.setStatus(StepStatus.RUNNING);
+                stepRepository.save(entity);
+
                 try {
                     step.getTask().run();
                     statuses.put(step.getName(), StepStatus.COMPLETED);
                     handler.sendUpdate(new ProgressMessage(step.getName(), StepStatus.COMPLETED));
+                    entity.setStatus(StepStatus.COMPLETED);
+                    stepRepository.save(entity);
+
                 } catch (Exception e) {
                     statuses.put(step.getName(), StepStatus.FAILED);
                     handler.sendUpdate(new ProgressMessage(step.getName(), StepStatus.FAILED));
+                    entity.setStatus(StepStatus.FAILED);
+                    stepRepository.save(entity);
+
                     return new WorkflowResult(false, step.getName(), statuses);
                 }
             } catch (IOException e) {
--- flowline-engine/src/main/java/com/flowline/workflow_engine/services/WorkflowService.java
+++ flowline-engine/src/main/java/com/flowline/workflow_engine/services/WorkflowService.java
@@ -1,13 +1,19 @@
 package com.flowline.workflow_engine.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flowline.workflow_engine.engine.Engine;
+import com.flowline.workflow_engine.engine.enums.StepStatus;
 import com.flowline.workflow_engine.engine.models.Step;
+import com.flowline.workflow_engine.engine.models.StepEntity;
 import com.flowline.workflow_engine.engine.models.WorkflowResult;
 import com.flowline.workflow_engine.handlers.ProgressWebSocketHandler;
+import com.flowline.workflow_engine.repos.StepRepository;
 import com.flowline.workflow_engine.steps.StepDefinition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,13 +22,35 @@ public class WorkflowService {
 
     private final ProgressWebSocketHandler handler;
 
+    private final StepRepository stepRepository;
+
+    private final ObjectMapper objectMapper;
+
     public WorkflowResult runWorkflow(List<StepDefinition> stepDefinitions) {
         List<Step> steps = stepDefinitions.stream()
                 .map(def -> new Step(def.getName(), def.toRun()))
                 .toList();
 
-        Engine engine = new Engine(handler);
-        return engine.run(steps);
+        List<StepEntity> entities = new ArrayList<>();
+
+        for (int i=0; i<stepDefinitions.size(); i++) {
+            StepDefinition def = stepDefinitions.get(i);
+            StepEntity entity = new StepEntity();
+
+            entity.setStepOrder(i);
+            try {
+                entity.setDefinitionJson(objectMapper.writeValueAsString(def));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+            entity.setStatus(StepStatus.NOT_STARTED);
+
+            entities.add(entity);
+            stepRepository.save(entity);
+        }
+
+        Engine engine = new Engine(handler, stepRepository);
+        return engine.run(steps, entities);
     }
 
 }
